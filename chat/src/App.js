fe7f409c151ea8721ce4b924201cb8ce7322aea9
ashimpoudyal98 <75020083+ashimpoudyal@users.noreply.github.com>
2023-07-13 22:20:42 +0545

    socket io implementation

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Join from "./component/Join/Join"
+
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <h1>Working</h1>
-    </div>
-  );
-}
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<Join />} />
+      <Route path="/chat" />
+    </Routes>
+</div>
+
+)}
+  
+    
+  
+
 
 export default App;
---
+++ chat/src/component/Join/Join.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Join.css"
+
+const Join = () => {
+  return (
+    <div className='JoinPage'>
+        <div className='JoinContainer'>
+            <h1>Kurakani</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Join
