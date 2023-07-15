160b32a0ef0b914001ff18d93763561bbb978cd0
ashimpoudyal98 <75020083+ashimpoudyal@users.noreply.github.com>
2023-07-15 21:57:48 +0545

    Complete project

--- chat/src/component/Chat/Chat.js
+++ chat/src/component/Chat/Chat.js
@@ -1,30 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {user} from "../Join/Join"
 import socketIO from "socket.io-client"
 import "./Chat.css"
 import sendLogo from "../../images/send.png"
+import Message from "../Message/Message"
+import ReactScrollToBottom from "react-scroll-to-bottom"
+import closeIcon from "../../images/closeIcon.png"
 const ENDPOINT = "http://localhost:4500/"
+
+
+let socket;
 
 const Chat = () => {
 
+    const [message, setMessage] = useState('')
+    const [messages,setMessages] = useState([])
+    const[id, setId] = useState('')
+const send=()=>{
+   
+    socket.emit('message',{message,id})
+    setMessage('');
+}
 
 useEffect(()=>{
-const socket = socketIO(ENDPOINT, {transports: ['websocket']})
+ socket = socketIO(ENDPOINT, {transports: ['websocket']})
 
 socket.on('connect',()=>{
     alert("Your server is connected")
+    setId(socket.id)
 })
 socket.emit('joined',{user})
 
 socket.on('welcome',(data)=>{
+   setMessages([...messages,data]);
     console.log(data.user,data.message)
 })
 
 socket.on('userJoined',(data)=>{
+   setMessages([...messages,data]);
     console.log(data.user,data.message)
 })
 
 socket.on('leave',(data)=>{
+    setMessages([...messages,data]);
     console.log(data.user,data.message)
 })
 
@@ -34,14 +52,29 @@ return()=>{
 }
 },[])
 
+useEffect(()=>{
+    socket.on('sendMessage',(data)=>{
+        setMessages([...messages,data]);
+        console.log(data.user,data.message,data.id)
+    })
+    return()=>{
+       socket.off()
+    }
+},[messages])
+
   return (
     <div className='chatPage'>
         <div className='chatContainer'>
-            <div className='header'></div>
-            <div className='chatBox'></div>
+            <div className='header'>
+                <h2>Kurakani</h2>
+                <a href='/'><img src={closeIcon} alt="close"/></a>
+            </div>
+            <ReactScrollToBottom className="chatBox">
+                    {messages.map((item, i) => <Message user={item.id === id ? '' : item.user} message={item.message} classs={item.id === id ? 'right' : 'left'} />)}
+                </ReactScrollToBottom>
             <div className='inputBox'>
-                <input type='text' id="chatInput"/>
-                <button className='sendBtn'><img src={sendLogo} alt="Send"/></button>
+                <input onKeyDown={(event)=>event.key === 'Enter'?send(): null} onChange={(e)=>setMessage(e.target.value)} type='text' id="chatInput" value={message}/>
+                <button onClick={send} className='sendBtn'><img src={sendLogo} alt="Send"/></button>
             </div>
         </div>
     </div>
